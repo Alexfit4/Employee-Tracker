1ffcd6fe9884189b8e0389323f0b24cb68bbdaa8
alexfit4 <alex.ashtiany@gmail.com>
2021-02-05 17:44:03 -0500

    created new folder to hold seed and data sql

--- sql/EmployeeTracker-Schema.sql
+++ sql/EmployeeTracker-Schema.sql
@@ -6,24 +6,27 @@ USE employee_trackerDB;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30),
+  name VARCHAR(30) NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30),
-  salary DECIMAL,
-  department_id INT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL NULL,
+  department_id INT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  role_id INT,
-  manager_id INT,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT NULL,
+  manager_id INT NULL,
+  FOREIGN KEY (role_id) REFERENCES department(id) NULL,
+  FOREIGN KEY (manager_id) REFERENCES department(id) NULL,
   PRIMARY KEY (id)
 );
 
@@ -34,18 +37,3 @@ VALUES
   ("Amir", 'Ashtiany'),
   ("Gabe", 'Johnson'),
   ("Rachael", 'Wanke');
-
-SELECT
-  *
-FROM
-  department;
-
-SELECT
-  *
-FROM
-  role;
-
-SELECT
-  *
-FROM
-  employee
---
+++ sql/seed.sql
@@ -0,0 +1,26 @@
+USE employee_trackerDB;
+
+INSERT INTO
+    department (name)
+VALUES
+    ('Engineer'),
+    ('Sales'),
+    ('Finance'),
+    ('Legal');
+
+INSERT INTO
+    role (title, salary, department_id)
+VALUES
+    ('Sales Lead', 100000, 2),
+    ('Salesperson', 80000, 2),
+    ('Lead Engineer', 150000, 1),
+    ('Software Engineer', 120000, 1),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+INSERT INTO
+    employee (first_name, last_name)
+VALUES
+    ("Johnnie", 'Simpson'),
+    ("Amir", 'Ashtiany'),
+    ("Gabe", 'Johnson'),
+    ("Rachael", 'Wanke');
